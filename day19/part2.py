e71ce44ccffb36ec7c6edbefbf31bcb723481f29
vampirecoffee <skye@vampirecoffee.net>
2023-12-20 14:08:51 -0800

    day 19 part 2 done

--- day19/part2.py
+++ day19/part2.py
@@ -4,10 +4,14 @@ from __future__ import annotations
 
 import argparse
 from collections import defaultdict
-from dataclasses import dataclass
+import itertools
+from dataclasses import dataclass, asdict
+import math
 from enum import Enum
 import re
 from typing import Optional, Union
+
+from tqdm import tqdm  # type: ignore[import-untyped]
 
 
 class Status(Enum):
@@ -43,6 +47,36 @@ class Part:
 
 
 @dataclass(frozen=True)
+class PartInterval:
+    x: tuple[int, int]
+    m: tuple[int, int]
+    a: tuple[int, int]
+    s: tuple[int, int]
+
+    def change_interval(
+        self, interval_name: str, new_interval: tuple[int, int]
+    ) -> PartInterval:
+        """Return a copy with the relevant interval changed."""
+        if interval_name == "x":
+            return PartInterval(new_interval, self.m, self.a, self.s)
+        elif interval_name == "m":
+            return PartInterval(self.x, new_interval, self.a, self.s)
+        elif interval_name == "a":
+            return PartInterval(self.x, self.m, new_interval, self.s)
+        elif interval_name == "s":
+            return PartInterval(self.x, self.m, self.a, new_interval)
+        else:
+            raise ValueError(f"Unrecognized interval name {interval_name}")
+
+    @property
+    def total(self) -> int:
+        """How many combinations does this interval contain?"""
+        return math.prod(
+            high - low + 1 for low, high in [self.x, self.m, self.a, self.s]
+        )
+
+
+@dataclass(frozen=True)
 class Condition:
     """Rule condition."""
 
@@ -50,24 +84,42 @@ class Condition:
     gt_lt: str
     val: int
 
-    def matches(self, part) -> bool:
+    def matches(self, part: Part) -> bool:
         """Does this condition match this part?"""
-        attr_val: int = 0
-        if self.attr == "x":
-            attr_val = part.x
-        elif self.attr == "m":
-            attr_val = part.m
-        elif self.attr == "a":
-            attr_val = part.a
-        elif self.attr == "s":
-            attr_val = part.s
-        else:
-            raise RuntimeError(f"attr should be x/m/a/s, not {self.attr}")
+        attr_val = asdict(part)[self.attr]
 
         if self.gt_lt == "<":
             return attr_val < self.val
         else:
             return attr_val > self.val
+
+    def interval_matches(
+        self, pi: PartInterval
+    ) -> tuple[Optional[PartInterval], Optional[PartInterval]]:
+        """For an interval, return the part that matches and the part that doesn't."""
+        low, high = asdict(pi)[self.attr]
+        # Case where no values match
+        if (self.gt_lt == ">" and self.val >= high) or (
+            self.gt_lt == "<" and self.val <= low
+        ):
+            return (None, pi)
+        # Case where all values match
+        if (self.gt_lt == ">" and self.val < low) or (
+            self.gt_lt == "<" and self.val > high
+        ):
+            return (pi, None)
+
+        match_interval: tuple[int, int]
+        no_match_interval: tuple[int, int]
+        if self.gt_lt == ">":
+            match_interval = (self.val + 1, high)
+            no_match_interval = (low, self.val)
+        else:
+            match_interval = (low, self.val - 1)
+            no_match_interval = (self.val, high)
+        mpi = pi.change_interval(self.attr, match_interval)
+        nmpi = pi.change_interval(self.attr, no_match_interval)
+        return mpi, nmpi
 
 
 _rule_re = re.compile(r"(x|m|a|s)(<|>)(\d+):(\w+)")
@@ -105,14 +157,31 @@ class Rule:
             s_in = s_in.strip()
             return cls(cond=None, _send_to=s_in)
 
-    def applies_to(self, part) -> bool:
+    def applies_to(self, part: Part) -> bool:
         """Does this rule apply to this part?"""
         if self.cond is None:
             return True
         return self.cond.matches(part)
 
+    def interval_applies_to(
+        self, pi: PartInterval
+    ) -> tuple[Optional[PartInterval], Optional[PartInterval]]:
+        """For an interval, return:
+        - the part of the interval that this rule applies to
+        - the part that doesn't
+        """
+        if self.cond is None:
+            return (pi, None)
+        return self.cond.interval_matches(pi)
+
 
 _workflow_re = re.compile(r"(\w+)\{(.*)\}")
+
+
+@dataclass(frozen=True)
+class IntervalTarget:
+    pi: PartInterval
+    to: Union[str, Status]
 
 
 @dataclass(frozen=True)
@@ -137,15 +206,42 @@ class Workflow:
                 return r.send_to
         raise RuntimeError("No rules applied :(")
 
+    def interval_apply(self, pi: PartInterval) -> list[IntervalTarget]:
+        """Apply this workflow to an interval."""
+        tgts: list[IntervalTarget] = []
+        for r in self.rules:
+            applies_to, does_not_apply_to = r.interval_applies_to(pi)
+            if applies_to is not None:
+                tgts.append(IntervalTarget(pi=applies_to, to=r.send_to))
+            if does_not_apply_to is None:
+                return tgts
+            pi = does_not_apply_to
+        return tgts
+
 
 def apply_workflows(workflows: dict[str, Workflow], part: Part) -> Status:
     """Apply a collection of workflows to this part."""
-    print("part:", part)
     cur: Union[str, Status] = "in"
     while not isinstance(cur, Status):
-        print("applying workflow named:", cur)
         cur = workflows[cur].apply(part)
     return cur
+
+
+def interval_apply_workflows(workflows: dict[str, Workflow], pi: PartInterval) -> int:
+    total_accepted = 0
+    queue: list[IntervalTarget] = [IntervalTarget(pi, "in")]
+    with tqdm(total=pi.total) as pbar:
+        while queue:
+            cur_step = queue.pop()
+            if isinstance(cur_step.to, Status):
+                if cur_step.to == Status.Accept:
+                    total_accepted += cur_step.pi.total
+                pbar.update(cur_step.pi.total)
+                continue
+            new_steps = workflows[cur_step.to].interval_apply(cur_step.pi)
+            for step in new_steps:
+                queue.append(step)
+    return total_accepted
 
 
 def score_parts(workflows: dict[str, Workflow], parts: list[Part]) -> int:
@@ -153,11 +249,32 @@ def score_parts(workflows: dict[str, Workflow], parts: list[Part]) -> int:
     out = 0
     for p in parts:
         if apply_workflows(workflows, p) == Status.Accept:
-            print("accepting part", p)
             out += p.rating
-        else:
-            print("rejecting part", p)
     return out
+
+
+def stupid_part_2_solution(workflows: dict[str, Workflow]) -> int:
+    """A very, very stupid solution to part 2."""
+    accepted = 0
+    total = 4000 * 4000 * 4000 * 4000
+    with tqdm(total=total) as pbar:
+        for x, m, a, s in tqdm(itertools.product(range(1, 4001), repeat=4)):
+            p = Part(x, m, a, s)
+            if apply_workflows(workflows, p) == Status.Accept:
+                accepted += 1
+            pbar.update(1)
+    return accepted
+
+
+def less_stupid_part_2(workflows: dict[str, Workflow]) -> int:
+    """Less stupid solution to part 2."""
+    pi = PartInterval(
+        x=(1, 4000),
+        m=(1, 4000),
+        a=(1, 4000),
+        s=(1, 4000),
+    )
+    return interval_apply_workflows(workflows, pi)
 
 
 def parse_file(filename: str) -> int:
@@ -179,7 +296,7 @@ def parse_file(filename: str) -> int:
         w = Workflow.from_str(s)
         workflows[w.name] = w
     parts = [Part.from_str(s) for s in part_strs]
-    return score_parts(workflows, parts)
+    return less_stupid_part_2(workflows)
 
 
 def main():
