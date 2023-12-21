8235e3a0b1d0cd91341be7f2fae342fba35ce645
vampirecoffee <skye@vampirecoffee.net>
2023-12-20 19:43:07 -0800

    this does not work either lol

--- day17/part1.py
+++ day17/part1.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 from copy import deepcopy
 import functools
+from typing import Optional
 from dataclasses import dataclass, field
 from typing import Any
 from collections import defaultdict
@@ -66,57 +67,112 @@ def take_step(row: int, col: int, step_dir: Dir) -> tuple[int, int]:
 
 @dataclass(frozen=True)
 class PathIsh:
-    going: Dir  # What direction we're going
-    for_steps: int  # How many steps we've taken in this direction
-    # for_steps should be >= 1 for all cells except the start
+    prev: Optional[Dir]
+    prev2: Optional[Dir]
+    prev3: Optional[Dir]
 
-    def __post_init__(self) -> None:
-        """Post-init checks."""
-        assert self.for_steps in range(0, 3 + 1)
+    @property
+    def valid(self) -> bool:
+        """True iff this PathIsh is valid.
 
-    def __eq__(self, other: Any) -> bool:
-        """Equality."""
-        return hash(self) == hash(other)
+        It's not valid to double back on yourself.
 
-    def __hash__(self) -> int:
-        """Hashing."""
-        if self.for_steps == 0:
-            return hash(self.for_steps) + hash(Dir.RIGHT)
-        return hash(self.for_steps) + hash(self.going)
+        It's not valid to have None as a step *after* a non-None step.
+        """
+        if self.prev is not None and self.prev.reverse() == self.prev2:
+            return False
+
+        if self.prev2 is not None:
+            if any(
+                (
+                    self.prev is None,
+                    self.prev2.reverse() == self.prev3,
+                )
+            ):
+                return False
+        if self.prev3 is not None:
+            if any(
+                (
+                    self.prev is None,
+                    self.prev2 is None,
+                ),
+            ):
+                return False
+
+        return True
 
     @classmethod
     def first(cls) -> PathIsh:
         """The starting 'path-ish'."""
-        return PathIsh(
-            going=Dir.RIGHT,
-            for_steps=0,
+        return PathIsh(None, None, None)
+
+    @property
+    def prev1(self) -> Optional[Dir]:
+        """Odds I type this by mistake: 100%."""
+        return self.prev
+
+    @property
+    def consec_steps(self) -> int:
+        """How many consecutive steps we've taken in this direction.
+
+        It'll be greater than 0, unless everything is None.
+        """
+        if self.prev is None:
+            return 0
+        steps = 1
+        if self.prev == self.prev2:
+            steps += 1
+            if self.prev == self.prev3:
+                steps += 1
+        return steps
+
+    @property
+    def at_max_consec_steps(self) -> bool:
+        """Is this showing the maximum number of consecutive steps?"""
+        return all(
+            (
+                self.prev is not None,
+                self.prev2 is not None,
+                self.prev3 is not None,
+                self.prev == self.prev2,
+                self.prev == self.prev3,
+            )
         )
 
     @property
     def valid_next_dirs(self) -> list[Dir]:
         """Return a list of valid next directions to go."""
-        if self.for_steps < 3:
-            return list(Dir)
-        else:
-            out = list(Dir)
-            out.remove(self.going)
-            return out
+        out = list(Dir)
+        if self.prev is not None:
+            if self.at_max_consec_steps:
+                out.remove(self.prev)
+            out.remove(self.prev.reverse())
+        return out
 
     def take_step(self, step: Dir) -> PathIsh:
         """If you take a step in a direction, what's that step's PathIsh?"""
-        if step == self.going:
-            return PathIsh(step, self.for_steps + 1)
-        return PathIsh(step, 1)
+        if self.prev == step:
+            assert not self.at_max_consec_steps
+        return PathIsh(step, self.prev, self.prev2)
 
+    @property
     def valid_next_paths(self) -> list[PathIsh]:
         """Which paths are valid next, given this one?"""
         next_dirs = self.valid_next_dirs
         return [self.take_step(d) for d in next_dirs]
 
 
+@functools.cache
 def _all_path_ish() -> list[PathIsh]:
-    """Get a list of all possible PathIsh-es."""
-    return [PathIsh(d, i) for d in list(Dir) for i in range(1, 4)]
+    """Get a list of all possible PathIsh-es.
+
+    Does NOT include the ones with None. Just fyi."""
+    out: list[PathIsh] = []
+    for a, b, c in itertools.product(list(Dir), repeat=3):
+        path = PathIsh(a, b, c)
+        if path.valid:
+            out.append(path)
+    return out
 
 
 # You're supposed to set maximum distance to "infinity" in all these graph problems
@@ -125,6 +181,35 @@ _inf = 99999999999999999999999999999999999999999999999
 
 _max_row = _inf
 _max_col = _inf
+
+
+@functools.cache
+def _paths_from_origin() -> dict[tuple[int, int], list[PathIsh]]:
+    """Follow all paths from the origin.
+
+    This *includes* the ones with None.
+    """
+    dir_opts: list[Optional[Dir]] = list(Dir)
+    dir_opts.append(None)
+    out: dict[tuple[int, int], list[PathIsh]] = {
+        (row, col): [] for row, col in itertools.product(list(range(0, 4)), repeat=2)
+    }
+    for a, b, c in itertools.product(dir_opts, repeat=3):
+        path = PathIsh(c, b, a)
+        if not path.valid:
+            continue
+        row, col = 0, 0
+        for step in [a, b, c]:
+            if step is None:
+                break
+            row, col = take_step(row, col, step)
+            if row < 0 or col < 0:
+                break  # if we go off the grid, that's not valid
+        if row < 0 or col < 0:
+            continue
+        out[(row, col)].append(path)
+
+    return out
 
 
 @functools.cache
@@ -137,24 +222,37 @@ def _only_valid_paths(
         # row 0: can't get here by going down (at all)
         # row 1: can't get here by going down 2 or 3 steps
         # row 2: can't get here by going down 3 steps
-        to_remove |= set(PathIsh(Dir.DOWN, i) for i in range(row, 4))
+        for pi in everything:
+            if pi.prev == Dir.DOWN and pi.consec_steps in range(row, 4):
+                to_remove.add(pi)
     for row in range(max_row, max_row - 3, -1):
         # max_row: can't get here by going up (at all)
         # max_row - 1: can't get here by going up 2 or 3 steps
         # max_row - 2: can't get here by going up 3 steps
         start_range = row - (max_row - 3)
-        to_remove |= set(PathIsh(Dir.UP, i) for i in range(start_range, 4))
+        for pi in everything:
+            if pi.prev == Dir.UP and pi.consec_steps in range(start_range, 4):
+                to_remove.add(pi)
     for col in range(0, 3):
         # col 0: can't get here by going right (at all)
         # etc
-        to_remove |= set(PathIsh(Dir.RIGHT, i) for i in range(col, 4))
+        for pi in everything:
+            if pi.prev == Dir.RIGHT and pi.consec_steps in range(col, 4):
+                to_remove.add(pi)
     for col in range(max_col, max_col - 3, -1):
         # max_col: can't get here by going left (at all)
         # etc
         start_range = col - (max_col - 3)
-        to_remove |= set(PathIsh(Dir.LEFT, i) for i in range(start_range, 4))
-    if row == 0 and col == 0:
-        everything.add(PathIsh.first())
+        for pi in everything:
+            if pi.prev == Dir.LEFT and pi.consec_steps in range(start_range, 4):
+                to_remove.add(pi)
+
+    # Special cases for the top-left corner
+    if row in range(0, 4) and col in range(0, 4):
+        special: list[PathIsh] = _paths_from_origin()[(row, col)]
+        for pi in special:
+            everything.add(pi)
+
     out = everything - to_remove
     return list(out)
 
@@ -169,6 +267,7 @@ class Block:
     dist_by_path: defaultdict[PathIsh, int] = field(
         default_factory=lambda: defaultdict(lambda: _inf)
     )
+    best_path: PathIsh = PathIsh.first()
 
     def __hash__(self) -> int:
         """Return the hash for this block."""
@@ -225,7 +324,9 @@ class Block:
     def best_path_str(self) -> str:
         """Return a string showing the best path on this cell."""
         bp = self._best_path
-        return bp.going.as_char()
+        if bp.prev is not None:
+            return bp.prev.as_char()
+        return str(self.cost)
 
     @property
     @functools.cache
@@ -235,8 +336,10 @@ class Block:
         Uses the 'best' path.
         """
         bp = self._best_path
-        backwards = bp.going.reverse()
-        return take_step(self.row, self.col, backwards)
+        if bp.prev is not None:
+            backwards = bp.prev.reverse()
+            return take_step(self.row, self.col, backwards)
+        return (self.row, self.col)
 
 
 # I'm only making this class so I don't lose track of my tuples
@@ -359,10 +462,10 @@ class City:
         # Where/how to go next?
 
         next_dist = vp.distance + block.cost
-        next_paths = vp.path.valid_next_paths()
+        next_paths = vp.path.valid_next_paths
         new_plans: list[VisitPlan] = []
         for path in next_paths:
-            new_row, new_col = take_step(vp.row, vp.col, path.going)
+            new_row, new_col = take_step(vp.row, vp.col, path.prev)
             if self.valid_coords(new_row, new_col):
                 new_vp = VisitPlan(
                     path=path,
@@ -380,19 +483,13 @@ class City:
         # Total possible number of plans:
         # number of rows
         # times number of columns
-        # times 3 (for 0-3 steps)
-        # times 4 (for directions: l/r/d/u)
-        # plus 1 (for the start plan, which is special)
-        max_plan_count = (
-            math.prod(
-                [
-                    len(self.blocks),
-                    len(self.blocks[0]),
-                    3,
-                    4,
-                ]
-            )
-            + 1
+        # times 5*5*5 (3 prev directions, 5 directions: r/l/d/u/None)
+        max_plan_count = math.prod(
+            [
+                len(self.blocks),
+                len(self.blocks[0]),
+                5 * 5 * 5,
+            ]
         )
         print("max plan count", max_plan_count)
         plans_executed: set[VisitPlan] = set()
