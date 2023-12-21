ad8d662dd35c02d7e428e93b33048a38b81c8e5e
vampirecoffee <skye@vampirecoffee.net>
2023-12-20 21:55:14 -0800

    wip ig

--- day18/part1.py
+++ day18/part1.py
@@ -1,7 +1,81 @@
 from __future__ import annotations
 
 import argparse
-from ..graph import Dir, Point
+from dataclasses import dataclass
+import re
+from graph import Dir, Point, enclosed_area, perimeter
+
+
+@dataclass(frozen=True)
+class RGB:
+    red: int
+    green: int
+    blue: int
+
+    @classmethod
+    def from_hex(cls, s: str) -> RGB:
+        """Convert a six-digit hex str into RGB."""
+        red = int(s[0:2], 16)
+        green = int(s[2:4], 16)
+        blue = int(s[4:6], 16)
+        return RGB(red, green, blue)
+
+
+_instr_re = re.compile(r"(\w) (\d) \(#(......)\)")
+
+
+@dataclass(frozen=True)
+class Instruction:
+    direction: Dir
+    n: int
+    color: RGB
+
+    @classmethod
+    def from_str(cls, s: str) -> Instruction:
+        """From a string. A 'line', perhaps"""
+        s = s.strip()
+        match = _instr_re.search(s)
+        if not match:
+            raise RuntimeError(f"Could not match {s} to instruction regex")
+        direction = Dir(match.group(1))
+        n = int(match.group(2))
+        rgb = RGB.from_hex(match.group(3))
+        return cls(direction, n, rgb)
+
+    def follow(self, start_point: Point) -> Point:
+        """Follow this instruction, starting from start_point."""
+        return start_point.go(self.direction, n=self.n)
+
+
+def follow_dig_plan(plan: list[Instruction]) -> list[Point]:
+    """Follow the dig plan and return a list of points in the polygon."""
+    p = Point(0, 0)
+    polygon = [p]
+    for instr in plan:
+        new_point = instr.follow(p)
+        polygon.append(new_point)
+        p = new_point
+    return polygon
+
+
+def lagoon_size(polygon: list[Point]) -> int:
+    """Find the size of our lagoon."""
+    enclosed = enclosed_area(polygon)
+    perim = perimeter(polygon)
+    return int(enclosed) + int(perim)
+
+
+def parse_file(filename: str) -> int:
+    """Parse file do thing"""
+    plan: list[Instruction] = []
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            plan.append(Instruction.from_str(line))
+    polygon = follow_dig_plan(plan)
+    return lagoon_size(polygon)
 
 
 def main():
--- graph.py
+++ graph.py
@@ -2,14 +2,15 @@
 
 from __future__ import annotations
 from dataclasses import dataclass
+import math
 from enum import Enum
 
 
 class Dir(Enum):
-    LEFT = 0
-    RIGHT = 1
-    UP = 2
-    DOWN = 3
+    LEFT = "L"
+    RIGHT = "R"
+    UP = "U"
+    DOWN = "D"
 
 
 @dataclass(frozen=True)
@@ -17,13 +18,70 @@ class Point:
     row: int
     col: int
 
-    def go(self, direction: Dir) -> Point:
+    def go(self, direction: Dir, n=1) -> Point:
         """From this point, go in a direction."""
         if direction == Dir.LEFT:
-            return Point(self.row, self.col - 1)
+            return Point(self.row, self.col - n)
         elif direction == Dir.RIGHT:
-            return Point(self.row, self.col + 1)
+            return Point(self.row, self.col + n)
         elif direction == Dir.UP:
-            return Point(self.row - 1, self.col)
+            return Point(self.row - n, self.col)
         elif direction == Dir.DOWN:
-            return Point(self.row + 1, self.col)
+            return Point(self.row + n, self.col)
+
+    def valid(self, max_height: int, max_width: int) -> bool:
+        """Is this point valid for a graph with the given max height+width?"""
+        return all(
+            (
+                self.row >= 0,
+                self.row <= max_height,
+                self.col >= 0,
+                self.col <= max_height,
+            )
+        )
+
+    def determinant(self, other: Point) -> int:
+        """Return the determinant of a 2x2 matrix of this and other.
+
+        Used for shoelace theorem stuff.
+        """
+        return (self.row * other.col) - (self.col * other.row)
+
+    def distance(self, other: Point) -> float:
+        """Distance between two points."""
+        row_diff_squared = (self.row - other.row) ** 2
+        col_diff_squared = (self.col - other.col) ** 2
+        return math.sqrt(row_diff_squared + col_diff_squared)
+
+
+def edges(polygon: list[Point]) -> list[tuple[Point, Point]]:
+    """Given a list of points in the polygon, get a list of edges."""
+    polygon = polygon + [polygon[0]]
+    return [(polygon[i], polygon[i + 1]) for i in range(len(polygon) - 1)]
+
+
+def enclosed_area(polygon: list[Point]) -> float:
+    """Find the enclosed area of this polygon."""
+    matrix_sum = sum(p1.determinant(p2) for p1, p2 in edges(polygon))
+    # The original formula is only valid going in one direction,
+    # and produces a negative result in the other.
+    # We could determine which way is counterclockwise ...
+    # or we could just calculate it twice lol
+    if matrix_sum < 0:
+        return enclosed_area(list(reversed(polygon)))
+    return matrix_sum / 2
+
+
+def count_enclosed_points(polygon: list[Point]) -> int:
+    """Count the number of integer points enclosed.
+
+    See: Pick's theorem.
+    """
+    area = enclosed_area(polygon)
+    boundary_points = len(polygon)
+    return int(area + 1 - (boundary_points / 2))
+
+
+def perimeter(polygon: list[Point]) -> float:
+    """Find the length of the perimeter of a polygon."""
+    return sum(p1.distance(p2) for p1, p2 in edges(polygon))
